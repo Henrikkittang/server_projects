df7a409468d375fdebda67dfcb086e58a60f20c3
Henrik <henrik.kittang@gmail.com>
2021-02-18 13:11:14 +0100

    fixing key issues

--- beer/main.js
+++ beer/main.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const fetch = require('node-fetch');
 const breweryDB = require('./breweryDB.js');
+require('dotenv').config()
 
-const brewdb = new breweryDB('f5a6e9a935f59c61b84739b8d2f150d9&p');
+const brewdb = new breweryDB(process.env.BREWERY_DB_KEY);
 
 const app = express();  
-app.listen(8000, () => {console.log(`listening at port 8000`)});
+app.listen(process.env.PORT, () => {console.log(`listening at port ${process.env.PORT}`)});
 app.use(express.static('public'));
 app.use(express.json({limit: '1mb'}));
 
@@ -29,7 +30,7 @@ app.get('/get_by_id/:id', async (request, response) =>{
 
 app.get('/location/:loc_name', async (request, response) =>{
     const loc_name = request.params['loc_name'];
-    const latlon = await fetch(`https://api.opencagedata.com/geocode/v1/geojson?q=${loc_name}&key=e56086c26fd943e7b7038c739616de1d`)
+    const latlon = await fetch(`https://api.opencagedata.com/geocode/v1/geojson?q=${loc_name}&key=${process.env.OPENCAGEDATA_KEY}`)
     json = await latlon.json();
     response.json(json);
 });
