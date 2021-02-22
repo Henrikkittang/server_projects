a204934021270935d197874f3bc5ca1d1e3b64a5
Henrik <henrik.kittang@gmail.com>
2021-02-22 20:08:58 +0100

    testing git_update module

--- file_upload/main.js
+++ file_upload/main.js
@@ -1,11 +1,14 @@
 
 const express = require('express');
 const fileUpload = require('express-fileupload');
+const git_update = require('kittang.git_update');
 
 const app = express();
 const server = app.listen(8000, () => {console.log('listening at port 8000')});
 app.use(express.static('public'));
 app.use(express.json({limit: '10mb'}));
+
+git_update.enableAuotmaticUpdate('https://github.com/Henrikkittang/server_projects');
 
 app.post('/file', (request, response) =>{
     console.log('Got data');
