59da29b47e302e54d2d66d9ff2b92fcf82b098bb
Henrikkittang <42532845+Henrikkittang@users.noreply.github.com>
2021-02-23 01:33:07 +0100

    Update main.js

--- file_upload/main.js
+++ file_upload/main.js
@@ -8,6 +8,8 @@ const server = app.listen(8000, () => {console.log('listening at port 8000')});
 app.use(express.static('public'));
 app.use(express.json({limit: '10mb'}));
 
+console.log('myspace cat');
+
 git_update.enableAuotmaticUpdate('https://github.com/Henrikkittang/server_projects')
 
 app.post('/file', (request, response) =>{
