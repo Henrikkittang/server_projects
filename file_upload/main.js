d5a78efd314a127702beb2311dfd27e66b233e9a
Henrik <henrik.kittang@gmail.com>
2021-02-23 01:27:55 +0100

    t

--- file_upload/main.js
+++ file_upload/main.js
@@ -8,7 +8,7 @@ const server = app.listen(8000, () => {console.log('listening at port 8000')});
 app.use(express.static('public'));
 app.use(express.json({limit: '10mb'}));
 
-git_update.enableAuotmaticUpdate('https://github.com/Henrikkittang/server_projects');
+git_update.enableAuotmaticUpdate('https://github.com/Henrikkittang/server_projects')
 
 app.post('/file', (request, response) =>{
     console.log('Got data');
